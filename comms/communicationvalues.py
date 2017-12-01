d951728d48afe411a29cc8a2e3bb3a5ddeb081a5
Eve Meier <eve.meier@stud.hslu.ch>
2017-12-01 16:41:58 +0100

    Fehler das CommunicationValue none ist behoben.. wir machen kein private singleton zeugs... dann geht Nächstes aktuelles Problem: AttributeError: 'module' object has no attribute 'Serial'... serial modul auf raspi scheint nicht zu funktionieren
    Auf Raspi:
    
    >>> import serial
    >>> serial.Serial
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
    AttributeError: 'module' object has no attribute 'Serial'

--- comms/communicationvalues.py
+++ comms/communicationvalues.py
@@ -23,111 +23,93 @@ from serialcommunicationhandler import SerialCommunicationHandler
 
 
 class CommunicationValues:
-    class __CommunicationValues:
-        def __init__(self):
-            # fileConfig(cfg.get_logging_config_fullpath())
-            # self.__log = logging.getLogger()
-            print("CommunicationValues init")
-            self.op_hello = None
-            self.op_state = None
-            self.op_position = "None"
-            self.__serialcom = None
-            self.timeout = 0.3
-            self.retrycounter = 20
-            time.sleep(1)
-            self.__start()
+    def __init__(self):
+        # fileConfig(cfg.get_logging_config_fullpath())
+        # self.__log = logging.getLogger()
+        print("CommunicationValues init")
+        self.op_hello = None
+        self.op_state = None
+        self.op_position = "None"
+        self.__serialcom = None
+        self.timeout = 0.3
+        self.retrycounter = 20
+        time.sleep(1)
+        self.__start()
 
-        def get_hello_blocking(self):
-            hellocounter = 0
-            while self.op_hello is None:
-                if hellocounter < self.retrycounter:
-                    time.sleep(self.timeout)
-                    hellocounter += 1
-                else:
-                    print("timeout hello blocking...")
-                    break
-            return self.op_hello
+    def get_hello_blocking(self):
+        hellocounter = 0
+        while self.op_hello is None:
+            if hellocounter < self.retrycounter:
+                time.sleep(self.timeout)
+                hellocounter += 1
+            else:
+                print("timeout hello blocking...")
+                break
+        return self.op_hello
 
-        def get_hello(self):
-            return self.op_hello
+    def get_hello(self):
+        return self.op_hello
 
-        def get_state(self):
-            return self.op_state
+    def get_state(self):
+        return self.op_state
 
-        def get_position(self):
-            return self.op_position
+    def get_position(self):
+        return self.op_position
 
-        def send_hello(self):
-            self.__serialcom.send("hello", 1)
-            hellocounter = 0
-            while self.op_hello is None:
-                if hellocounter < self.retrycounter:
-                    time.sleep(self.timeout)
-                    self.__serialcom.send("hello", 1)
-                    hellocounter += 1
-                else:
-                    print("hello message not acknowledged")
-                    break
+    def send_hello(self):
+        self.__serialcom.send("hello", 1)
+        hellocounter = 0
+        while self.op_hello is None:
+            if hellocounter < self.retrycounter:
+                time.sleep(self.timeout)
+                self.__serialcom.send("hello", 1)
+                hellocounter += 1
+            else:
+                print("hello message not acknowledged")
+                break
 
-        def send_state(self):
-            self.__serialcom.send("state", 1)
-            statecounter = 0
-            while self.op_state is None:
-                if statecounter < self.retrycounter:
-                    time.sleep(self.timeout)
-                    self.__serialcom.send("state", 1)
-                    statecounter += 1
-                else:
-                    print("state signal not acknowledged")
-                    break
+    def send_state(self):
+        self.__serialcom.send("state", 1)
+        statecounter = 0
+        while self.op_state is None:
+            if statecounter < self.retrycounter:
+                time.sleep(self.timeout)
+                self.__serialcom.send("state", 1)
+                statecounter += 1
+            else:
+                print("state signal not acknowledged")
+                break
 
-        def send_position(self, position):
-            self.__serialcom.send("position", position)
-            positioncounter = 0
-            while self.op_position is None:
-                if positioncounter < self.retrycounter:
-                    time.sleep(self.timeout)
-                    self.__serialcom.send("position", position)
-                    positioncounter += 1
-                else:
-                    print("position not acknowledged")
-                    break
+    def send_position(self, position):
+        self.__serialcom.send("position", position)
+        positioncounter = 0
+        while self.op_position is None:
+            if positioncounter < self.retrycounter:
+                time.sleep(self.timeout)
+                self.__serialcom.send("position", position)
+                positioncounter += 1
+            else:
+                print("position not acknowledged")
+                break
 
-        def __handleoperations(self):
-            while True:
-                operation, value = self.__serialcom.receive()
-                print("got op:" + str(operation) + " val: " + str(value))
-                if operation == "hello":
-                    if value == '1':
-                        self.op_hello = 1
-                elif operation == "state":
-                    if value == '1':
-                        self.op_state = 1
-                elif operation == "position":
-                    self.op_position = str(value)
-                else:
-                    print("got unknown operation: " + str(operation) + " with val: " + str(value))
+    def __handleoperations(self):
+        while True:
+            operation, value = self.__serialcom.receive()
+            print("got op:" + str(operation) + " val: " + str(value))
+            if operation == "hello":
+                if value == '1':
+                    self.op_hello = 1
+            elif operation == "state":
+                if value == '1':
+                    self.op_state = 1
+            elif operation == "position":
+                self.op_position = str(value)
+            else:
+                print("got unknown operation: " + str(operation) + " with val: " + str(value))
 
-        def __start(self):
-            self.__serialcom = SerialCommunicationHandler().start()
-            thread = Thread(target=self.__handleoperations, args=())
-            thread.daemon = True
-            thread.start()
-            return self
-
-
-    instance = None
-
-
-    def __new__(cls, *args, **kwargs):
-        if CommunicationValues.instance is None:
-            CommunicationValues.instance = CommunicationValues.__CommunicationValues()
-        return CommunicationValues.instance
-
-
-    def __getattr__(self, name):
-        return getattr(self.instance, name)
-
-
-    def __setattr__(self, name):
-        return setattr(self.instance, name)
+    def __start(self):
+        self.__serialcom = SerialCommunicationHandler().start()
+        thread = Thread(target=self.__handleoperations, args=())
+        thread.daemon = True
+        thread.start()
+        return self
--- comms/serialcommunicationhandler.py
+++ comms/serialcommunicationhandler.py
@@ -17,105 +17,93 @@ import logging
 import serial
 import queue
 import time
-#import config.confighandler as cfg
+# import config.confighandler as cfg
 
 from threading import Thread
+
+
 # from logging.config import fileConfig
 
 
 class SerialCommunicationHandler(object):
-    class __SerialCommunicationHandler:
-        def __init__(self):
-            #fileConfig(cfg.get_logging_config_fullpath())
-            #self.__log = logging.getLogger()
-            self.serialcom = None
-            self.data_send = queue.Queue()
-            self.data_receive = queue.Queue()
-            self.__initserialcom()
+    def __init__(self):
+        # fileConfig(cfg.get_logging_config_fullpath())
+        # self.__log = logging.getLogger()
+        self.serialcom = None
+        self.data_send = queue.Queue()
+        self.data_receive = queue.Queue()
+        self.__initserialcom()
 
-        def __initserialcom(self):
-            """
-            This function will initialize the corresponding GPIO-Pins for serial
-            """
-            print ("Serial communication initialization started")
-            self.serialcom = serial.Serial()
-            self.serialcom.baudrate = 9600
-            self.serialcom.port = '/dev/serial0'
-            self.serialcom.open()
+    def __initserialcom(self):
+        """
+        This function will initialize the corresponding GPIO-Pins for serial
+        """
+        print ("Serial communication initialization started")
+        self.serialcom = serial.Serial()
+        self.serialcom.baudrate = 9600
+        self.serialcom.port = '/dev/serial0'
+        self.serialcom.open()
 
-        def __txhandle(self):
-            try:
-                while self.serialcom.is_open:
-                    time.sleep(0.05)  # wait 50ms to prevent buffer overflow on FRDM
-                    while not self.data_send.empty():
-                        currentqueueitem = self.data_send.get()
-                        self.serialcom.write(currentqueueitem.encode('utf-8'))
-                        print("value sent: " + str(currentqueueitem.strip('\n')))
-                else:
-                    self.__reconnectcommunication()
-            except serial.SerialException as e:
-                print("serial connection lost..." + str(e.strerror))
-                if self.serialcom.is_open():
-                    self.__reconnectcommunication()
+    def __txhandle(self):
+        try:
+            while self.serialcom.is_open:
+                time.sleep(0.05)  # wait 50ms to prevent buffer overflow on FRDM
+                while not self.data_send.empty():
+                    currentqueueitem = self.data_send.get()
+                    self.serialcom.write(currentqueueitem.encode('utf-8'))
+                    print("value sent: " + str(currentqueueitem.strip('\n')))
+            else:
+                self.__reconnectcommunication()
+        except serial.SerialException as e:
+            print("serial connection lost..." + str(e.strerror))
+            if self.serialcom.is_open():
+                self.__reconnectcommunication()
 
-        def __rxhandle(self):
-            try:
-                while self.serialcom.is_open:
-                    time.sleep(0.01)  # wait 10ms to retrieve next value to reduce CPU load
-                    currentreceiveitem = self.serialcom.readline()
-                    currentqueueitem_op, currentqueueitem_value = self.__encodestring(currentreceiveitem)
-                    self.data_receive.put([currentqueueitem_op, currentqueueitem_value])
-                    print("value received: " + str(currentqueueitem_op) + "," + str(currentqueueitem_value))
-                else:
-                    self.__reconnectcommunication()
-            except serial.SerialException as e:
-                print("serial connection lost..." + str(e))
-                if self.serialcom.is_open:
-                    self.__reconnectcommunication()
+    def __rxhandle(self):
+        try:
+            while self.serialcom.is_open:
+                time.sleep(0.01)  # wait 10ms to retrieve next value to reduce CPU load
+                currentreceiveitem = self.serialcom.readline()
+                currentqueueitem_op, currentqueueitem_value = self.__encodestring(currentreceiveitem)
+                self.data_receive.put([currentqueueitem_op, currentqueueitem_value])
+                print("value received: " + str(currentqueueitem_op) + "," + str(currentqueueitem_value))
+            else:
+                self.__reconnectcommunication()
+        except serial.SerialException as e:
+            print("serial connection lost..." + str(e))
+            if self.serialcom.is_open:
+                self.__reconnectcommunication()
 
-        def __reconnectcommunication(self):
-            self.start()
+    def __reconnectcommunication(self):
+        self.start()
 
-        def send(self, operation, value):
-            decodedvalue = self.__decodestring(operation, value)
-            self.data_send.put(decodedvalue + '\n')
+    def send(self, operation, value):
+        decodedvalue = self.__decodestring(operation, value)
+        self.data_send.put(decodedvalue + '\n')
 
-        def receive(self):
-            operation, opval = self.data_receive.get()
-            return operation, opval
+    def receive(self):
+        operation, opval = self.data_receive.get()
+        return operation, opval
 
-        def start(self):
-            t_read = Thread(target=self.__rxhandle, args=())
-            t_write = Thread(target=self.__txhandle, args=())
-            t_read.daemon = True
-            t_write.daemon = True
-            t_read.start()
-            t_write.start()
-            return self
+    def start(self):
+        t_read = Thread(target=self.__rxhandle, args=())
+        t_write = Thread(target=self.__txhandle, args=())
+        t_read.daemon = True
+        t_write.daemon = True
+        t_read.start()
+        t_write.start()
+        return self
 
-        def __encodestring(self, inputvalue):
-            try:
-                operation, opval = inputvalue.split(b',')
-                operation = operation.decode()
-                opval = opval.decode()
-                return operation.rstrip(), opval.rstrip()
-            except:
-                print("org: " + str(inputvalue))
-                return "ERROR", "ERROR"
+    def __encodestring(self, inputvalue):
+        try:
+            operation, opval = inputvalue.split(b',')
+            operation = operation.decode()
+            opval = opval.decode()
+            return operation.rstrip(), opval.rstrip()
+        except:
+            print("org: " + str(inputvalue))
+            return "ERROR", "ERROR"
 
-        def __decodestring(self, operation, opval):
-            combinedvalue = operation + "," + str(opval)
-            return combinedvalue
-
-    instance = None
-
-    def __new__(cls, *args, **kwargs):
-        if not SerialCommunicationHandler.instance:
-            SerialCommunicationHandler.instance = SerialCommunicationHandler.__SerialCommunicationHandler()
-        return SerialCommunicationHandler.instance
-
-    def __getattr__(self, name):
-        return getattr(self.instance, name)
-
-    def __setattr__(self, name):
-        return setattr(self.instance, name)
+    def __decodestring(self, operation, opval):
+        combinedvalue = operation + "," + str(opval)
+        return combinedvalue
