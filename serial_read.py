26a464fde5a573ae85d63e7eebc997a92391b50d
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-02-23 11:30:42 +0100

    We have comms :D

--- serial_read.py
+++ serial_read.py
@@ -1,13 +1,11 @@
-import time
 import serial
-ser = serial.Serial(
-	port='/dev/ttyAMA0',
-	baudrate = 9600,
-	parity=serial.PARITY_NONE,
-	stopbits=serial.STOPBITS_ONE,
-	bytesize=serial.EIGHTBITS,
-	timeout=1
-)
+
+ser = serial.Serial('/dev/serial0')
+ser.parity =serial.PARITY_NONE
+ser.stopbits = serial.STOPBITS_ONE
+ser.bytesize = serial.EIGHTBITS
+ser.timeout = 1
+
 counter=0
 while 1:
 	x=ser.readline()
--- serial_write.py
+++ serial_write.py
@@ -1,16 +1,17 @@
 import time
 import serial
-ser = serial.Serial(
-	port='/dev/ttyAMA0',
-	baudrate = 9600,
-	parity=serial.PARITY_NONE,
-	stopbits=serial.STOPBITS_ONE,
-	bytesize=serial.EIGHTBITS,
-	timeout=1
-)
+
+# UART serial interface has changed
+# https://raspberrypi.stackexchange.com/questions/45570/how-do-i-make-serial-work-on-the-raspberry-pi3
+ser = serial.Serial('/dev/serial0')
+ser.parity =serial.PARITY_NONE
+ser.stopbits = serial.STOPBITS_ONE
+ser.bytesize = serial.EIGHTBITS
+ser.timeout = 1
+
 counter=0
 
 while 1:
-	ser.write('Write counter: %d \n'%(counter))
+	ser.write('Count %d time(s)\n'%(counter))
 	time.sleep(1)
 	counter += 1
